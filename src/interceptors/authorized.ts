a3f42faefc324f59c4d09992060a6f26a71d1013
Leandro <leandrolupano@hotmail.com>
2023-03-02 21:48:52 +0100

    ✅ Add auth test without finish

--- src/interceptors/authorized.ts
+++ src/interceptors/authorized.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Response } from 'express';
 import { RequestPlus } from './logged.js';
 
 export function authorized(
